b1f3f39adf0986bfb7fbe6189326c01511c1d1d3
Paulo <pauloviniciustavaressouza@gmail.com>
2022-01-18 14:09:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const inputCep = document.getElementById('cep');
+const btnCep = document.getElementById('btnCep');
+const resultadoCep = document.querySelector('.resultadoCep');
+
+btnCep.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  const cep = inputCep.value;
+  console.log(cep);
+  buscaCep(cep);
+}
+
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then((response) => response.text())
+    .then((body) => (resultadoCep.innerText = body));
+}
